8e8f2d5733bc35b0c601abbaeeec8214fdc9f3f9
colinsubler <csubler@purdue.edu>
2025-11-11 11:13:00 -0500

    solved

--- prisma/migrations/20251111161223_init/migration.sql
+++ prisma/migrations/20251111161223_init/migration.sql
@@ -1,4 +1,15 @@
 -- CreateTable
+CREATE TABLE "Students" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "major" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "gpa" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "Students_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Profiles" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
